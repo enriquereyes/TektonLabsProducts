c1de468d2ac02b416b44a42f810456d01ec7f194
Luis Enrique Reyes Serrano <lenrique.reyesse@gmail.com>
2022-02-13 22:54:51 -0600

    Models

---
+++ TektonLabsProducts.Core/Models/Product.cs
@@ -0,0 +1,10 @@
+namespace TektonLabsProducts.Core.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public int InStock { get; set; }    
+    }
+}
